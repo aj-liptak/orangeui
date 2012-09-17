c58d34fd22e2a9f624c2a6925347b3029b5c6e23
Kevin Kinnebrew <kevin.kinnebrew@gmail.com>
2012-09-17 11:38:47 -0500

    Adding api docs
    Signed-off-by: Kevin Kinnebrew <kevin.kinnebrew@gmail.com>

---
+++ api/api.js
@@ -0,0 +1,51 @@
+YUI.add("yuidoc-meta", function(Y) {
+   Y.YUIDoc = { meta: {
+    "classes": [
+        "Application",
+        "Binding",
+        "Browser",
+        "Cache",
+        "Class",
+        "Collection",
+        "Deferred",
+        "EventHandle",
+        "EventTarget",
+        "Events",
+        "Loader",
+        "Location",
+        "Log",
+        "Model",
+        "Module",
+        "Promise",
+        "Queue",
+        "Service",
+        "Storage",
+        "View",
+        "ViewController"
+    ],
+    "modules": [
+        "Core",
+        "Data",
+        "HTML5",
+        "UI"
+    ],
+    "allModules": [
+        {
+            "displayName": "Core",
+            "name": "Core"
+        },
+        {
+            "displayName": "Data",
+            "name": "Data"
+        },
+        {
+            "displayName": "HTML5",
+            "name": "HTML5"
+        },
+        {
+            "displayName": "UI",
+            "name": "UI"
+        }
+    ]
+} };
+});
